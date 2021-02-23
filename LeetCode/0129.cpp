85adf328098bc084a078d87aa0d51208845d2cf8
Yang Dong <yangdong0729@outlook.com>
2021-02-23 20:06:50 +0800

    LeetCode

---
+++ LeetCode/0129.cpp
@@ -0,0 +1,24 @@
+﻿struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int sumNumbers(TreeNode *root) {
+        return _sumNumbers(root, 0);
+    }
+
+    int _sumNumbers(TreeNode *root, int number) {
+        if (!root)
+            return 0;
+        number = number * 10 + root->val;
+        if (!root->left && !root->right)
+            return number;
+        return _sumNumbers(root->left, number) + _sumNumbers(root->right, number);
+    }
+};
---
+++ LeetCode/0700.java
@@ -0,0 +1,31 @@
+﻿class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null || root.val == val)
+            return root;
+        // 递归框架
+        if (root.val < val)
+            return searchBST(root.right, val);
+        if (root.val > val)
+            return searchBST(root.left, val);
+        return null;
+    }
+}
