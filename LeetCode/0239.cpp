48e37766e2467489a33e7f7c78df75958eb49ce6
Yang Dong <yangdong0729@outlook.com>
2021-02-23 11:55:22 +0800

    0239

---
+++ LeetCode/0239.cpp
@@ -0,0 +1,118 @@
+﻿#include <vector>
+#include <deque>
+#include <string>
+#include <sstream>
+#include <iostream>
+
+using namespace std;
+
+class MonotonicQueue {
+private:
+    deque<int> q;
+
+public:
+    void push(int n) {
+        // 将前面小于自己的都删除
+        while (!q.empty() && q.back() < n) {
+            q.pop_back();
+        }
+        q.push_back(n);
+    }
+
+    int max() {
+        // 队头元素是最大的
+        return q.front();
+    }
+
+    void pop(int n) {
+        if (n == q.front()) {
+            q.pop_front();
+        }
+    }
+};
+
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int> &nums, int k) {
+        MonotonicQueue window;
+        vector<int> ans;
+        for (int i = 0; i < nums.size(); i++) {
+            if (i < k - 1) {
+                // 先填满窗口的前 k - 1
+                window.push(nums[i]);
+            } else {
+                // 窗口向前滑动，加入新数字
+                window.push(nums[i]);
+                // 记录当前窗口的最大值
+                ans.push_back(window.max());
+                // 移出旧数字
+                window.pop(nums[i - k + 1]);
+            }
+        }
+        return ans;
+    }
+};
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+                    return !isspace(ch);
+                }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+                    return !isspace(ch);
+                }).base(),
+                input.end());
+}
+
+vector<int> stringToIntegerVector(string input) {
+    vector<int> output;
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    stringstream ss;
+    ss.str(input);
+    string item;
+    char delim = ',';
+    while (getline(ss, item, delim)) {
+        output.push_back(stoi(item));
+    }
+    return output;
+}
+
+int stringToInteger(string input) {
+    return stoi(input);
+}
+
+string integerVectorToString(vector<int> list, int length = -1) {
+    if (length == -1) {
+        length = list.size();
+    }
+
+    if (length == 0) {
+        return "[]";
+    }
+
+    string result;
+    for (int index = 0; index < length; index++) {
+        int number = list[index];
+        result += to_string(number) + ", ";
+    }
+    return "[" + result.substr(0, result.length() - 2) + "]";
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        vector<int> nums = stringToIntegerVector(line);
+        getline(cin, line);
+        int k = stringToInteger(line);
+
+        vector<int> ret = Solution().maxSlidingWindow(nums, k);
+
+        string out = integerVectorToString(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
