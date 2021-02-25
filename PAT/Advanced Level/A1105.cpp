2fee2ee654e815b0aa1b30a1da725eb42e0b7f7c
Yang Dong <yangdong0729@outlook.com>
2021-02-25 11:35:40 +0800

    PAT

---
+++ PAT/Advanced Level/A1105.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <array>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    int num;
+    cin >> num;
+
+    vector<int> arr(num);
+    for (int i = 0; i < num; ++i) cin >> arr[i];
+    sort(arr.begin(), arr.end(), greater<int>());
+
+    int m = 0, n = 0;
+    for (int i = 1; i * i <= num; ++i) {
+        if (num % i == 0) {
+            n = i;
+            m = num / n;
+        }
+    }
+
+    vector<vector<int>> spiral(m, vector<int>(n));
+    for (int i = 0, j = 0; j < num; ++i) {
+        for (int a = i; a < n - i && j < num; ++a) spiral[i][a] = arr[j++];
+        for (int b = i + 1; b < m - i && j < num; ++b) spiral[b][n - i - 1] = arr[j++];
+        for (int c = n - i - 2; c >= i && j < num; --c) spiral[m - i - 1][c] = arr[j++];
+        for (int d = m - i - 2; d > i && j < num; --d) spiral[d][i] = arr[j++];
+    }
+
+
+    for (int i = 0; i < m; ++i) {
+        for (int j = 0; j < n; ++j) {
+            cout << spiral[i][j];
+            if (j != n - 1) cout << " ";
+        }
+        cout << "\n";
+    }
+}
---
+++ PAT/Advanced Level/A1106.cpp
@@ -0,0 +1,57 @@
+﻿#include <algorithm>
+#include <iomanip>
+#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+struct {
+    double price;
+    vector<int> child;
+} node[100005];
+
+int main() {
+    int n;
+    double r;
+    cin >> n >> node[0].price >> r;
+
+    r = 1 + r / 100;
+
+    for (int i = 0; i < n; ++i) {
+        int k;
+        cin >> k;
+        for (int j = 0; j < k; ++j) {
+            int c;
+            cin >> c;
+            node[i].child.push_back(c);
+        }
+    }
+
+    queue<int> q;
+    q.push(0);
+
+    int cnt = 0;
+    double minPrice;
+    while (!q.empty()) {
+        int top = q.front();
+        q.pop();
+
+        vector<int> &child = node[top].child;
+        if (child.size() == 0) { // 找到了一个零售商
+            if (cnt == 0) {
+                minPrice = node[top].price;
+                cnt = 1;
+            } else if (minPrice == node[top].price)
+                ++cnt;
+            else  // 可以直接退出
+                break;
+        } else
+            for (int i = 0, size = child.size(); i < size; ++i) {
+                node[child[i]].price = node[top].price * r;
+                q.push(child[i]);
+            }
+    }
+
+    cout << fixed << setprecision(4) << minPrice << " " << cnt;
+}
