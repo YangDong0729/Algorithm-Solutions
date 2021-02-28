62a0f138fc9b36847d8b4a116a59544b8962e5dd
Yang Dong <yangdong0729@outlook.com>
2021-02-28 11:12:55 +0800

    数据结构

---
+++ 浙大版《数据结构（第 2 版）》/习题2.2 数组元素循环左移的最优解.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int gcd(int a, int b) {
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+int main() {
+    int N, M;
+    cin >> N >> M;
+    M %= N;
+
+    vector<int> Number(N);
+    for (int i = 0; i < N; i++) {
+        cin >> Number[i];
+    }
+
+    int Loop = gcd(M, N);
+    for (int i = 0; i < Loop; i++) {
+        int head = i, tmp = Number[i];
+        do {
+            int nxt = (i - M + N) % N;
+            swap(Number[nxt], tmp);
+            i = nxt;
+        } while (i != head);
+    }
+
+    for (int i = 0; i < N; ++i) {
+        cout << Number[i] << (i == N - 1 ? "\n" : " ");
+    }
+}
